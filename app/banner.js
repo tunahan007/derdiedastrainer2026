50d204428a9c4aa775e479ac83dd5d6a42937723
tunahan007 <hilmi_demirkaya@yahoo.de>
2025-11-09 17:17:44 +0100

    last state

--- app/banner.js
+++ app/banner.js
@@ -10,9 +10,8 @@ export default function ABanner() {
     <View>
       {
         <BannerAd
-          //unitId={"ca-app-pub-3818927199662677/3221934793"}
-          // TEST
-          unitId={"ca-app-pub-3940256099942544/9214589741"}
+          unitId={"ca-app-pub-3818927199662677/3221934793"}
+          // TESTunitId={"ca-app-pub-3940256099942544/9214589741"}
           size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
         />
       }
